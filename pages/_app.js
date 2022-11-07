d55862a96f2014576189e0ce669bb1227ff47e5b
MarkusWedler <markuswedler8@gmail.com>
2022-11-07 11:52:34 +1000

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,17 @@
 // style
 import '../styles/main.css'
-// loader
+// next
+import Head from 'next/head'
+// components
 import Loader from '../layout/Loader'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Loader/>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.svg" />
+      </Head>
       <Component {...pageProps} />
     </>
   )
