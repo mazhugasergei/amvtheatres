4361458ba36c14b8e2117340b4016a136acc8d5d
Markus Wedler <markuswedler8@gmail.com>
2022-12-09 19:13:30 +1000

    added apple-touch-icon

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="shortcut icon" type="image/svg" href="/favicon.svg" />
+        <link rel="apple-touch-icon" href="/favicon.svg" />
         <meta name="description" content="Find movies near you, view show times, watch movie trailers and buy movie tickets. AMV Theatres has the newest movies near you." />
       </Head>
       <Loader/>
